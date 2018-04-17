c2bdd23ccc3c7821decfcaca3daec1524f825a2e
Osvaldo Daibert <odaibert@microsoft.com>
2018-04-17 10:59:28 -0700

    Unit Test added

---
+++ src/vsts-sf-devops/SampleWebAppIIS/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StoreApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
