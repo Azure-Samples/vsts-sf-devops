9fe0dfa32edab246b719def027f0f00d358facfc
Oguz Pastirmaci <oguzp@microsoft.com>
2018-04-19 21:36:13 -0700

    Update updateContainerImage.ps1

--- scripts/updateContainerImage.ps1
+++ scripts/updateContainerImage.ps1
@@ -1,3 +1,5 @@
 $path = "$Env:SYSTEM_DEFAULTWORKINGDIRECTORY" + "\src\TesteBuild\testBuildSF\ApplicationPackageRoot\SampleWebAppPkg\ServiceManifest.xml"
 
-(Get-Content $path).replace("MyContainer", "$Env:SFImageNAme") | Set-Content $path
+(Get-Content $path).replace('MyContainer', $Env:SFImageName) | Set-Content $path
+
+Get-Content $path
